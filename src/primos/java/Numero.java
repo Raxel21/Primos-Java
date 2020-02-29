5802a29c20031cf7543004443dd6afa23b6e1f6a
Celvyn <yisusparley@gmail.com>
2020-02-28 22:15:04 -0600

    Números primos y la suma de ellos encontrado

---
+++ src/primos/java/Numero.java
@@ -0,0 +1,22 @@
+package primos.java;
+
+/**
+ *
+ * @author Celvyn
+ */
+public class Numero {
+    /**
+     * Función para determinar si un número es primo
+     * @param s número entero
+     * @return verdadero si el número no es primo, falso si el número es primo     
+     */
+    public static boolean esPrimo(int s) {
+        for ( int m = 3; m < s; m += 2) {
+            int residuo = s % m;
+            if (residuo == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/primos/java/PrimosJava.java
+++ src/primos/java/PrimosJava.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package primos.java;
+
+import java.util.*;
 
 /**
  *
  * @author Celvyn
  */
-public class PrimosJava {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class PrimosJava {       
+    
+    public static void main(String[] args) {                                       
+        ArrayList<Integer> primos = new ArrayList();
+        primos.add(2);
+        
+        Scanner input = new Scanner(System.in);
+        
+        int numero;
+        int sumaPrimos = 0;
+        
+        System.out.println("Ingresa un número");
+        
+        numero = input.nextInt();
+        
+        int numeroAbs = Math.abs(numero);
+        
+        for ( int s = 3; s <= numeroAbs; s += 2) {
+            if (Numero.esPrimo(s)) {
+                continue;
+            }
+            primos.add(s);
+        }
+        
+        // sumaPrimos = primos.stream().map((e) -> e).reduce(sumaPrimos, Integer::sum);
+        for (int e: primos) {
+            sumaPrimos += e;
+        }
+        System.out.println(primos);
+        System.out.println(sumaPrimos);
+        
+        if (Numero.esPrimo(5)) {
+            System.out.println("El número no es primo");
+        } else {
+            System.out.println("El número es primo");
+        }
     }
     
 }
